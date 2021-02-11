ea6a6a3eee2c4aec7b3f75e1f1c8ccff21d7c965
Joseph Davis <joethebartenderpdx@gmail.com>
2021-02-10 16:57:11 -0800

    rough calc

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-// import functions and grab DOM elements
+// grab DOM elements
+const firstNumber = document.getElementById('first-number');
+const secondNumber = document.getElementById('second-number');
+const firstNumberB = document.getElementById('first-number-b');
+const secondNumberB = document.getElementById('second-number-b');
+const firstNumberC = document.getElementById('first-number-c');
+const secondNumberC = document.getElementById('second-number-c');
+const firstNumberD = document.getElementById('first-number-d');
+const secondNumberD = document.getElementById('second-number-d');
+const answer1 = document.getElementById('answer-1');
+const answer2 = document.getElementById('answer-2');
+const answer3 = document.getElementById('answer-3');
+const answer4 = document.getElementById('answer-4');
+const doMath = document.getElementById('button');
 
-// initialize state
+doMath.addEventListener('click', () =>{
+    answer1.textContent = Number(firstNumber.value) + Number(secondNumber.value);
+});
+doMath.addEventListener('click', () =>{
+    answer2.textContent = Number(firstNumberB.value) - Number(secondNumberB.value);
+});
+doMath.addEventListener('click', () =>{
+    answer3.textContent = Number(firstNumberC.value) * Number(secondNumberC.value);
+});
+doMath.addEventListener('click', () =>{
+    answer4.textContent = Number(firstNumberD.value) / Number(secondNumberD.value);
+});
 
-// set event listeners to update state and DOM
+
+
+
+// set event listeners to update on each button, do math and update DOM
+
